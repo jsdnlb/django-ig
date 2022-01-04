a1caedeca6d75cca4d0496feacf7c0532e1181bc
jsdnlb <danielb.alzate1@gmail.com>
2022-01-04 07:51:40 -0500

    Using template system

--- posts/views.py
+++ posts/views.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render
 
 # Django
-from django.http import HttpResponse
+from django.shortcuts import render
 # Utilities
 from datetime import datetime
 
@@ -10,40 +10,37 @@ from datetime import datetime
 # Global var
 posts = [
     {
-        'name':'Mont Blac',
-        'user': 'Daniel Buitrago',
+        'title': 'Mont Blanc',
+        'user': {
+            'name': 'Yésica Cortés',
+            'picture': 'https://picsum.photos/60/60/?image=1027'
+        },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture' :'https://picsum.photos/200/200/?image=903'
+        'photo': 'https://picsum.photos/800/600?image=1036',
     },
     {
-        'name':'Mont Blac',
-        'user': 'Daniel Buitrago',
+        'title': 'Via Láctea',
+        'user': {
+            'name': 'Christian Van der Henst',
+            'picture': 'https://picsum.photos/60/60/?image=1005'
+        },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture' :'https://picsum.photos/200/200/?image=903'
+        'photo': 'https://picsum.photos/800/800/?image=903',
     },
     {
-        'name':'Mont Blac',
-        'user': 'Daniel Buitrago',
+        'title': 'Nuevo auditorio',
+        'user': {
+            'name': 'Uriel (thespianartist)',
+            'picture': 'https://picsum.photos/60/60/?image=883'
+        },
         'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture' :'https://picsum.photos/200/200/?image=903'
-    },
-    {
-        'name':'Mont Blac',
-        'user': 'Daniel Buitrago',
-        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
-        'picture' :'https://picsum.photos/200/200/?image=903'
+        'photo': 'https://picsum.photos/500/700/?image=1076',
     }
 ]
 
 # Create your views here.
+
+
 def list_posts(request):
     # I make a loop to populate the initial page.
-    content = []
-    for post in posts:
-        content.append("""
-        <p>{name}</p>
-        <p><small>{user} - <i>{timestamp}</i></small></p>
-        <figure><img src="{picture}"></figure>
-        """.format(**post))
-    post = [1,2,3]
-    return HttpResponse('<br>'.join(content))
+    return render(request, 'feed.html', {'posts': posts})
